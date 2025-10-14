937635ce6f54ecdbeefef9789345efe97fc7c011
santyleo <l.sanchezcorani@colegiocalasanz.edu.ar>
2025-10-14 13:18:54 -0300

    Add files via upload

---
+++ sa.py
@@ -0,0 +1,20 @@
+lista=["Buenos Aires","Entre Rios","Córdoba","Chubut" ]
+
+provincias=input("¿que provincia quieres saber si es real?")
+
+for n in provincias:
+    if n =="Buenos Aires":
+        print("La Plata")
+     if n =="Entre Rios":
+         print("Parana")
+      if n =="Córdoba":
+          print("Córdoba")              
+       if n =="Entre Rios":
+           print("Rawson")
+     else:
+         print("verificar")
+           
+           
+                   
+
+        
