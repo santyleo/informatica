010a7708f12bfe8e907eb1a845a4d9ad1f78c7b7
santyleo <l.sanchezcorani@colegiocalasanz.edu.ar>
2025-10-21 13:16:07 -0300

    Add files via upload

---
+++ n.py
@@ -0,0 +1,10 @@
+cadena=input("dame la cadena de caracteres entre comillas por favor")
+acum=0
+for c in cadena:
+    if c==" ":
+       acum=+1
+    if acum>0:
+        
+    
+      
+print (acum)    
---
+++ t.py
@@ -0,0 +1,8 @@
+diccionario={}
+clave=int(input("hasta que numero quiere saber el cuadrado"))
+for d in range(1,clave+1):
+    diccionario[d]= d**2
+
+print(diccionario)
+
+
